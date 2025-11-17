5846e8718da68c2ea081ec7f3764c153e4ffe2ac
Dan Lilienblum <dlilienblum@gmail.com>
2025-11-17 23:47:31 +0900

    Refactor `createUseIsFeatureEnabled` to simplify type extraction for flag names, enhancing type safety and clarity in the hook's implementation.

--- src/react.ts
+++ src/react.ts
@@ -2,13 +2,9 @@ import React from "react";
 import type { FlareFlags } from "./client.js";
 import type { FlagName } from "./types.js";
 
-type ExtractFlagName<T> = T extends FlareFlags<infer TFlagName>
-  ? TFlagName
-  : never;
-
 export const createUseIsFeatureEnabled =
-  <TFlags extends FlareFlags<FlagName>>(instance: FlareFlags<FlagName>) =>
-  (flagName: ExtractFlagName<TFlags>) => {
+  <TFlagName extends FlagName>(instance: FlareFlags<TFlagName>) =>
+  (flagName: TFlagName) => {
     const getSnapshot = () => instance.isEnabled(flagName);
     return React.useSyncExternalStore(
       instance.subscribe,
